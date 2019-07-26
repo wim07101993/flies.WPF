1e782f2dc13fd62e8757a6532bfcfff3bd08ef1c
wim <van.laer.wim@gmail.com>
2019-07-26 20:00:26 +0200

    movee everything

---
+++ Flies.Service/IParticipantService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Flies.Service
+{
+    public interface IParticipantService
+    {
+        Task<IList<ParticipantPOCO>> Participants { get; }
+    }
+}
---
+++ Flies.Service/ParticipantPOCO.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Flies.Service
+{
+    public class ParticipantPOCO
+    {
+        [JsonProperty("id")]
+        public uint Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("score")]
+        public ushort Score { get; set; }
+    }
+}
