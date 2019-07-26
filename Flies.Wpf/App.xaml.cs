6ecebc18923c24e660d8171e8782f9508ed17990
wim <van.laer.wim@gmail.com>
2019-07-26 21:28:26 +0200

    moved mainwindow to views

--- Flies.Wpf/App.xaml.cs
+++ Flies.Wpf/App.xaml.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Flies.Wpf.Views;
 using System.Windows;
 
 namespace Flies.Wpf
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+        }
     }
 }
